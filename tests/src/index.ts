0fe8e991ce168fbc4d00b7df47800f52c42f592e
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2021-02-04 13:22:05 +0900

    emit_signal testing

--- tests/src/index.ts
+++ tests/src/index.ts
@@ -1,140 +1,43 @@
-import { Orchestrator, Config } from '@holochain/tryorama';
+import { Config, Orchestrator, InstallAgentsHapps } from '@holochain/tryorama';
+import { ScenarioApi } from '@holochain/tryorama/lib/api';
+import path from 'path'
 
-const orchestrator = new Orchestrator();
-
-// Configure a conductor with two identical DNAs,
-// differentiated by UUID, nicknamed "alice" and "bobbo"
-const config = Config.gen({
-    alice: Config.dna('../testlab.dna.gz', null),
-    bobbo: Config.dna('../testlab.dna.gz', null),
-  })
 
 const delay = (ms) => new Promise((r) => setTimeout(r, ms))
 
+const config = Config.gen();
+let testlabDNA = path.join('../testlab.dna.gz');
 
-orchestrator.registerScenario('Scenario 1: alice create and alice direclt get links from zomeone', async (s, t) => {
-  // spawn the conductor process
-  const { conductor } = await s.players({ conductor: config });
-  await conductor.spawn();
+const installAgent: InstallAgentsHapps = [
+  [[testlabDNA]],
+  [[testlabDNA]],
+] 
 
-  const [dna_hash_1, agent_pubkey_alice] = conductor.cellId('alice');
+const orchestrator = new Orchestrator();
 
-  await conductor.call("alice", "zomeone", "create_foo_and_link_to_path", null);
-  await delay(1000);
-
-  // Works with no problem
-  let links = await conductor.call("alice", "zomeone", "get_links_from_path", "bar");
-  console.log("tatsuya sato 1");
-  console.log(links)
-
-  t.deepEqual(links.length, 1);
-});
-
-orchestrator.registerScenario('Scenario 2: bobby create and alice get links from zomeone', async (s, t) => {
-  // spawn the conductor process
-  const { conductor } = await s.players({ conductor: config })
-  await conductor.spawn()
-
-  const [dna_hash_2, agent_pubkey_bobby] = conductor.cellId('bobbo');
-
-  await conductor.call("bobbo", "zomeone", "create_foo_and_link_to_path", null);
-  await delay(1000);
-
-  // works with no problem too
-  let links = await conductor.call("alice", "zomeone", "get_links_from_path", "bar");
-
-  t.deepEqual(links.length, 1);
-});
-
-orchestrator.registerScenario('Scenario 3: alice create and alice remotely get links from zometwo', async (s, t) => {
-  // spawn the conductor process
-  const { conductor } = await s.players({ conductor: config })
-  await conductor.spawn()
-
-  const [dna_hash_1, agent_pubkey_alice] = conductor.cellId('alice');
-
-  await conductor.call("alice", "zomeone", "create_foo_and_link_to_path", null);
-  await delay(1000);
-
-  // this works with no problem as well
-  let links = await conductor.call("alice", "zometwo", "get_links_from_path_from_zomeone", null);
-
-  t.deepEqual(links.length, 1);
-
-});
-
-// orchestrator.registerScenario('Scenario 4: bobby create and alice get links from path remotely from zometwo', async (s, t) => {
-//   // spawn the conductor process
-//   const { conductor } = await s.players({ conductor: config })
-//   await conductor.spawn()
-
-//   const [dna_hash_2, agent_pubkey_bobby] = conductor.cellId('bobby');
-
-//   await conductor.call("bobby", "zomeone", "create_foo_and_link_to_path", null);
-//   await delay(100000);
-
-//   // now this doesn't work because the links return an empty array inside the get_author function
-//   let links = await conductor.call("alice", "zometwo", "get_links_from_path_from_zomeone", null);
-
-//   t.deepEqual(links.length, 1);
-// });
-
-// orchestrator.registerScenario('Scenario 5: bobby creates and alice get links from foo from zomeone', async (s, t) => {
-//   // spawn the conductor process
-//   const { conductor } = await s.players({ conductor: config })
-//   await conductor.spawn()
-
-//   const [dna_hash_2, agent_pubkey_bobby] = conductor.cellId('bobby');
-
-//   await conductor.call("bobby", "zomeone", "create_and_link_foo", null);
-//   await delay(10000);
-
-//   // now this doesn't work because the links return an empty array inside the get_author function
-//   let links = await conductor.call("alice", "zometwo", "get_links_from_foo_from_zomeone", null);
-
-//   t.deepEqual(links.length, 1);
-// });
-
-// orchestrator.registerScenario('Scenario 6: alice creates and alice calls remotely to bobbos cell to get links ', async (s, t) => {
-//   // spawn the conductor process
-//   const { conductor } = await s.players({ conductor: config })
-//   await conductor.spawn()
-
-//   const [dna_hash_2, agent_pubkey_bobby] = conductor.cellId('bobbo');
-//   // bobbo grants capability for function get_links_from_foo to be called unrestrictedly.
-//   await conductor.call("bobbo", "zomeone", "create_cap_grant", null);
-
-//   // alice commit Foo("foo") and Foo("foofoo"), and create a link (base: Foo("foo"), target: Foo("foofoo").
-//   await conductor.call("bobbo", "zomeone", "create_and_link_foo", null);
-//   await delay(10000);
-
-//   // alice calls function `caller` which invoke `call_remote` which invokes get_links_from_foo in bobbo's cell
-//   let links = await conductor.call("alice", "zomeone", "caller", agent_pubkey_bobby);
-//   console.log("get links remotely");
-//   console.log(links);
-
-//   // this fails as links returned from alice's call_remote to bobbo's to be 0
-//   t.deepEqual(links.length, 1);
-// });
-
-// orchestrator.registerScenario('Scenario 7: linking private entries and getting the link', async (s, t) => {
-//   // spawn the conductor process
-//   const { conductor } = await s.players({ conductor: config })
-//   await conductor.spawn()
-
-//   const [dna_hash_2, agent_pubkey_bobby] = conductor.cellId('bobbo');
-//   await conductor.call("alice", "zomeone", "create_foofoo_and_link", null);
-
-//   await delay(10000);
+// orchestrator.registerScenario('link from public to private', async (s: ScenarioApi, t) => {
+//   const [conductor] = await s.players([config]);
+//   const [[alice_lobby_happ], [bobby_lobby_happ]] = await conductor.installAgentsHapps(installAgent);
+//   const [alice_cell] = alice_lobby_happ.cells;
+//   const [bobby_cell] = bobby_lobby_happ.cells;
   
-  // let links = await conductor.call("alice", "zomeone", "get_links_from_foofoo", null);
-  // console.log("get private links");
-  // console.log(links);
-
-  // this fails as links returned from alice's call_remote to bobbo's to be 0
-  // t.deepEqual(links.length, 1);
+//   await alice_cell.call('zomeone', 'create_and_link_foo', null);
+//   let foofoo = await alice_cell.call('zomeone', 'get_foo_foo', null);
+//   console.log("---------is link working?---------")
+//   console.log(foofoo);
 // });
 
+orchestrator.registerScenario('link from private to public', async (s: ScenarioApi, t) => {
+  const [conductor] = await s.players([config]);
+  const [[alice_lobby_happ], [bobby_lobby_happ]] = await conductor.installAgentsHapps(installAgent);
+  const [alice_cell] = alice_lobby_happ.cells;
+  const [bobby_cell] = bobby_lobby_happ.cells;
+  
+  await alice_cell.call('zomeone', 'create_and_link_foofoo', null);
+  let foo = await alice_cell.call('zomeone', 'get_foo', null);
+  console.log("---------is link working?---------")
+  console.log(foo);
+});
 
-orchestrator.run()
+orchestrator.run();
 
--- zomes/zomeone/src/lib.rs
+++ zomes/zomeone/src/lib.rs
@@ -1,162 +1,74 @@
 use hdk3::prelude::*;
 
-#[derive(Deserialize, Serialize, SerializedBytes)]
-#[derive(Clone)]
-enum Bat {
-    Foo,
-    Bar(String),
-    Baz
-}
-
 // used for testing the links in call_remote
 #[hdk_entry(id = "foo", visibility = "public" )]
 #[derive(Clone, Debug)]
 struct Foo (String);
 
-// used for testing enum in entries
-#[hdk_entry(id = "bar", visibility = "public" )]
-#[derive(Clone)]
-struct Bar {
-    foo: bool
-}
-
 // used for testing linking private entries
-#[hdk_entry(id = "foofoo", visibility = "public" )]
+#[hdk_entry(id = "foofoo", visibility = "private" )]
 #[derive(Clone, Debug)]
 struct FooFoo (String);
-
-// used for testing linking private entries
-#[hdk_entry(id = "barbar", visibility = "public" )]
-#[derive(Clone, Debug)]
-struct BarBar (String);
-
-#[derive(Deserialize, Serialize, SerializedBytes)]
-struct StringWrapper(String);
 
 entry_defs![
     Path::entry_def(),
     Foo::entry_def(),
-    Bar::entry_def(),
-    FooFoo::entry_def(),
-    BarBar::entry_def()
+    FooFoo::entry_def()
 ];
 
 #[hdk_extern]
-fn create_bar(_: ()) -> ExternResult<Bar> {
-    let bar = Bar {
-        foo: false
-    };
-    create_entry!(bar.clone())?;
-    Ok(bar)
-}
-
-#[hdk_extern]
-fn create_cap_grant(_: ()) -> ExternResult<()> {
-    let mut functions: GrantedFunctions = HashSet::new();
-    functions.insert((zome_info!()?.zome_name, "get_links_from_foo".into()));
-    create_cap_grant!(
-        CapGrantEntry {
-            tag: "".into(),
-            // empty access converts to unrestricted
-            access: ().into(),
-            functions,
-        }
-    )?;
-    Ok(())
-}
-
-#[hdk_extern]
-fn create_foo_and_link_to_path(_: ()) -> ExternResult<Foo> {
-    let foo = Foo("foo".to_owned());
-    create_entry!(&foo)?;
-    let path = Path::from("bar".to_owned());
-    path.ensure()?;
-    create_link!(
-        hash_entry!(&path)?,
-        hash_entry!(&foo)?,
-        LinkTag::new("foo->path".to_owned())
-    )?;
-    Ok(foo)
-}
-
-#[hdk_extern]
-fn get_links_from_path(input: StringWrapper) -> ExternResult<Links> {
-    let path = Path::from(input.0);
-    let path_hash = hash_entry!(&path)?;
-    let links = get_links!(path_hash.clone())?;
-    debug!("links are {:#?}", links)?;
-    debug!("hash of the path is {:#?}", path_hash)?;
-    Ok(links)
-}
-
-#[hdk_extern]
-fn create_and_link_foo(_: ()) -> ExternResult<()> {
-    let base = Foo("foo".to_string());
-    let target = Foo("foofoo".to_string());
-    let base_hash = hash_entry!(&base)?;
-    let target_hash = hash_entry!(&base)?;
-    let tag = LinkTag::new("foos");
-    create_entry!(&base)?;
-    create_entry!(&target)?;
-    create_link!(base_hash, target_hash, tag)?;
-    Ok(())
-}
-
-#[hdk_extern]
-fn get_links_from_foo(_: ()) -> ExternResult<Links> {
-    let base = Foo("foo".to_owned());
-    debug!("base for get_author_of_foo {:#?}", base.clone())?;
-    let base_hash = hash_entry!(&base)?;
-    debug!("base_hash for get_author_of_foo {:#?}", base_hash.clone())?;
-    let links = get_links!(base_hash)?;
-    debug!("links for get_author_of_foo {:#?}", links.clone())?;
-    Ok(links)
-}
-
-#[hdk_extern]
-fn caller(agent: AgentPubKey) -> ExternResult<Links> {
-    let function_name = zome::FunctionName("get_links_from_foo".to_owned());
-
-    match call_remote!(
-        agent, 
-        "zomeone".into(),
-        function_name, 
-        None,
-        ().try_into()?
-    )? {
-        ZomeCallResponse::Ok(output) => {
-            let sb = output.into_inner();
-            let links: Links = sb.try_into()?;
-            Ok(links)
-        },
-        ZomeCallResponse::Unauthorized => {
-            Err(HdkError::Wasm(WasmError::Zome(
-                "this agent has no proper authorization".to_owned()
-            )))
-        },
+fn emit_signal_20(_: ()) -> ExternResult<()> {
+    let foo = Foo("testing emit_signal".to_owned());
+    for _ in 0..20 {
+        emit_signal(foo.clone())?;
     }
+    Ok(())
 }
 
-#[hdk_extern]
-fn create_foofoo_and_link(_: ()) -> ExternResult<FooFoo> {
-    let foofoo = FooFoo("foofoo".to_owned());
-    let barbar = BarBar("barbar".to_owned());
-    let base_hash = hash_entry!(&foofoo)?;
-    let target_hash = hash_entry!(&barbar)?;
-    create_entry!(&foofoo)?;
-    create_entry!(&barbar)?;
-    create_link!(
-        base_hash,
-        target_hash,
-        LinkTag::new("foofoo->barbar")
-    )?;
-    Ok(foofoo)
-}
+// #[hdk_extern]
+// fn create_and_link_foo(_: ()) -> ExternResult<()> {
+//     let base = Foo("foo".to_string());
+//     let target = FooFoo("foofoo".to_string());
+//     let base_hash = hash_entry(&base)?;
+//     let target_hash = hash_entry(&target)?;
+//     let tag = LinkTag::new("foos");
+//     create_entry(&base)?;
+//     create_entry(&target)?;
+//     create_link(base_hash, target_hash, tag)?;
+//     Ok(())
+// }
 
-#[hdk_extern]
-fn get_links_from_foofoo(_: ()) -> ExternResult<Links> {
-    let foofoo = FooFoo("foofoo".to_owned()); 
-    let base_hash = hash_entry!(&foofoo)?;
-    let links = get_links!(base_hash)?;
-    Ok(links)
-}
+// #[hdk_extern]
+// fn get_foo_foo(_: ()) -> ExternResult<FooFoo> {
+//     let base_hash = hash_entry(&Foo("foo".to_string()))?;
+//     let tag = LinkTag::new("foos");
+//     let links = get_links(base_hash, Some(tag))?.into_inner();
+//     let get_option = GetOptions::latest();
+//     let target = get(links[0].target.clone(), get_option)?.unwrap().into_inner();
+//     let foofoo: FooFoo = target.1.to_app_option()?.unwrap(); 
+//     Ok(foofoo)
+// }
+
+// #[hdk_extern]
+// fn create_and_link_foofoo(_: ()) -> ExternResult<()> {
+//     let base = FooFoo("foofoo".to_string());
+//     let base_hash = hash_entry(&base)?;
+//     let target = Foo("foo".to_string());
+//     let target_hash = hash_entry(&target)?;
+//     let tag = LinkTag::new("foos");
+//     create_entry(&base)?;
+//     create_entry(&target)?;
+//     create_link(base_hash, target_hash, tag)?;
+//     Ok(())
+// }
+
+// #[hdk_extern]
+// fn get_foo(_: ()) -> ExternResult<Foo> {
+//     let base_hash = hash_entry(&FooFoo("foofoo".to_string()))?;
+//     let tag = LinkTag::new("foos");
+//     let links = get_links(base_hash, Some(tag))?.into_inner();
+//     let get_option = GetOptions::latest();
+//     let target = get(links[0].target.clone(), get_option)?.unwrap().into_inner();
+//     let foo: Foo = target.1.to_app_option()?.unwrap(); 
+//     Ok(foo)
+// }
